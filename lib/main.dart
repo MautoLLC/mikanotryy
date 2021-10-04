aa685255491dfcbd5edd22c7479228a230fe49bd
Joseph Lteif <JosephLteif@outlook.com>
2021-10-04 12:00:13 +0300

    removed flutter downloader

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:mymikano_app/services/pushNotificationService.dart';
 import 'package:mymikano_app/utils/main/store/AppStore.dart';
 import 'package:mymikano_app/views/screens/SplashScreen.dart';
@@ -18,9 +17,6 @@ Future<void> _messageHandler(RemoteMessage message) async {
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await FlutterDownloader.initialize(
-      debug: false // optional: set false to disable printing logs to console
-      );
   await Firebase.initializeApp();
   await dotenv.load(fileName: ".env");
   FirebaseMessaging.onBackgroundMessage(_messageHandler);
