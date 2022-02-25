5236642f5f12d98798cce61bf46dac017e01fbb4
Joseph Lteif <JosephLteif@outlook.com>
2022-02-25 12:13:09 +0200

    Trending Item now clickable

--- lib/views/widgets/HorizontalItemElement.dart
+++ lib/views/widgets/HorizontalItemElement.dart
@@ -14,92 +14,91 @@ class HorizontalItemElement extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 10),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          border:
-              Border(bottom: BorderSide(color: lightBorderColor, width: 1))),
-      child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              alignment: Alignment.center,
-              decoration: boxDecoration(
-                  radius: 33,
-                  showShadow: true,
-                  bgColor: mainGreyColorTheme.withOpacity(0.3)),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(33),
-                child: commonCacheImageWidget(
-                  product.Image,
-                  60,
-                  width: 80,
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            width: 15,
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  product.Name,
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontFamily: "Poppins",
-                    color: mainBlackColorTheme,
-                  ),
-                  // overflow: TextOverflow.ellipsis,
-                ),
-                SizedBox(
-                  height: 11,
-                ),
-                Text(
-                  product.Code,
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontFamily: "Poppins",
-                    color: mainGreyColorTheme,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Spacer(),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  "\$${product.Price}",
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontFamily: "Poppins",
-                    color: mainBlackColorTheme,
-                  ),
-                ),
-                SizedBox(
-                  height: 7,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    // TODO logic
-                    Navigator.of(context).push(MaterialPageRoute(
+    return GestureDetector(
+      onTap: (){
+        Navigator.of(context).push(MaterialPageRoute(
                         builder: (context) => ProductDetailsPage(
                               product: product,
                             )));
-                  },
-                  child: commonCacheImageWidget(ic_Cart, 24),
-                )
-              ],
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 10),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border:
+                Border(bottom: BorderSide(color: lightBorderColor, width: 1))),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                alignment: Alignment.center,
+                decoration: boxDecoration(
+                    radius: 33,
+                    showShadow: true,
+                    bgColor: mainGreyColorTheme.withOpacity(0.3)),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(33),
+                  child: commonCacheImageWidget(
+                    product.Image,
+                    60,
+                    width: 80,
+                  ),
+                ),
+              ),
             ),
-          )
-        ],
+            SizedBox(
+              width: 15,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    product.Name,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontFamily: "Poppins",
+                      color: mainBlackColorTheme,
+                    ),
+                    // overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(
+                    height: 11,
+                  ),
+                  Text(
+                    product.Code,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontFamily: "Poppins",
+                      color: mainGreyColorTheme,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    "\$${product.Price}",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontFamily: "Poppins",
+                      color: mainBlackColorTheme,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 7,
+                  ),
+                  commonCacheImageWidget(ic_Cart, 24)
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
