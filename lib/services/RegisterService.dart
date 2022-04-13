df98fe68524e71086307fd1533907dc4d53c9a73
petersokhn015 <201720143@ua.edu.lb>
2022-04-13 16:12:46 +0300

    fixed a bug when the user is registered as first time in flow of api config settings
    fixed a bug when a user registers in a brand new account and the api config settings causes error, also fixed a bug related to the success or error message in the api config settings regarding fetching generator ids

--- lib/services/RegisterService.dart
+++ lib/services/RegisterService.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:mymikano_app/utils/strings.dart';
 import 'package:mymikano_app/views/screens/SignInScreen.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
@@ -26,6 +27,19 @@ Register(String username, String firstname, String lastname, String email,
       });
 
   if (response.statusCode == 201) {
+    
+    if(prefs.getBool(prefs_DashboardFirstTimeAccess) == null){
+      await prefs.setBool(prefs_DashboardFirstTimeAccess, true);
+    }
+
+    if(prefs.getString(prefs_ApiConfigurationOption) == null){
+      await prefs.setString(prefs_ApiConfigurationOption, 'lan');
+    }
+
+    if(prefs.getInt(prefs_RefreshRate) == null){
+      await prefs.setInt(prefs_RefreshRate, 60);
+    }
+
     Fluttertoast.showToast(
         msg: "User created successfully ! ",
         toastLength: Toast.LENGTH_SHORT,
