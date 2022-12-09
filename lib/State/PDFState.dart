94d76060ef1e5800c4389c0dbd20de247813cddf
Joseph Lteif <JosephLteif@outlook.com>
2022-12-09 11:03:59 +0200

    Changed to download folder after permission granted

--- lib/State/PDFState.dart
+++ lib/State/PDFState.dart
@@ -18,34 +18,37 @@ class PDFState extends ChangeNotifier {
   void DownloadPDF(String Path, String Code) async {
     try {
       PermissionStatus status = await Permission.storage.request();
-      toast("Pdf is downloading, please wait.");
-      Directory? directory;
-      if (Platform.isIOS) {
-        directory = await getApplicationDocumentsDirectory();
-      } else {
-        directory = await getExternalStorageDirectory();
-      }
-      // List<Directory>? directories = await getExternalStorageDirectories();
-
-      try {
-        toggleProgressBarVisibility(true);
-        notifyListeners();
-        Dio dio = Dio();
-        Response response = await dio.download(
-          Path,
-          "${directory!.path.toString()}/${Code}.pdf",
-          onReceiveProgress: (count, total) => progress = total,
-        );
-        if (response.statusCode == 200) {
-          toast("Downloaded Successfully");
+      if (status == PermissionStatus.granted) {
+        toast("Pdf is downloading, please wait.");
+        Directory? directory;
+        if (Platform.isIOS) {
+          directory = await getApplicationDocumentsDirectory();
         } else {
-          toast("Failed to Download");
+          // directory = await getExternalStorageDirectory();
+          directory = Directory("/storage/emulated/0/Download");
         }
-      } catch (e) {
-        debugPrint(e.toString());
-      } finally {
-        toggleProgressBarVisibility(false);
-        notifyListeners();
+        // List<Directory>? directories = await getExternalStorageDirectories();
+
+        try {
+          toggleProgressBarVisibility(true);
+          notifyListeners();
+          Dio dio = Dio();
+          Response response = await dio.download(
+            Path,
+            "${directory!.path.toString()}/${Code}.pdf",
+            onReceiveProgress: (count, total) => progress = total,
+          );
+          if (response.statusCode == 200) {
+            toast("Downloaded Successfully");
+          } else {
+            toast("Failed to Download");
+          }
+        } catch (e) {
+          debugPrint(e.toString());
+        } finally {
+          toggleProgressBarVisibility(false);
+          notifyListeners();
+        }
       }
     } catch (e) {
       rethrow;
